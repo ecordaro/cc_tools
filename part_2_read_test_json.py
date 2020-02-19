72b8aaf6b2fdda4606470df726f7ee0270e7c129
ecordaro <33436015+ecordaro@users.noreply.github.com>
2020-02-18 22:38:35 -0500

    phase 2 project 2
    commit test_data.json, added code to part_2

--- part_2_read_test_json.py
+++ part_2_read_test_json.py
@@ -5,7 +5,13 @@ import json
 def make_game_library_from_json( json_data ):
     #Initialize a new GameLibrary
     game_library = test_data.GameLibrary()
-
+    for game in json_data:
+        gamenum = json_data[game]
+        plat = gamenum["platform"]
+        for i in plat:
+            newplat = test_data.Platform(plat["name"], plat["launch year"])
+        new_game = test_data.Game(title=gamenum["title"], platform=newplat,year=gamenum["Year"])
+        game_library.add_game(new_game)
     ### Begin Add Code Here ###
     #Loop through the json_data
         #Create a new Game object from the json_data by reading
@@ -20,7 +26,11 @@ def make_game_library_from_json( json_data ):
 
 #Part 2
 input_json_file = "data/test_data.json"
-
+with open(input_json_file, "r") as reader:
+    #load the JSON data and store it in the variable family_json_data
+    json_data = json.load(reader)
+ans= make_game_library_from_json(json_data)
+print(ans)
 ### Begin Add Code Here ###
 #Open the file specified by input_json_file
 #Use the json module to load the data from the file
