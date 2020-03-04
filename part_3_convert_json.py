6c2a04bc829066cbd016c9b9420879d741d020b5
ecordaro <33436015+ecordaro@users.noreply.github.com>
2020-03-04 04:06:17 -0500

    Update part_3_convert_json.py
    commented code

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -8,16 +8,19 @@ import cc_classes as ccc
 
 
 def make_level(data):
+	#initialize levelpack
 	levelpack = ccc.CCLevelPack()
+	#initialize crucial aspects of level for each level in input file
 	for l in data:
 		level = ccc.CCLevel()
 		level.level_number = l["level_number"]
 		level.time = l["time"]
 		level.num_chips = l["num_chips"]
 		level.upper_layer = l["upper_layer"]
-
+		#go through optional fields and add fields to self.optional fields
 		for field in l["optional_fields"]:
 			ftype = field["field_type"]
+			#condition on field type, add these values to the level
 			if ftype == "title":
 				title = ccc.CCMapTitleField(field["value"])
 				level.add_field(title)
